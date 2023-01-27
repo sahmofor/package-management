9c7c33ca7c2fddae7b52042b072ef7e1bce3083a
sahmofor <59724047+sahmofor@users.noreply.github.com>
2023-01-27 14:44:14 -0500

    Update jenkins-ubuntu-installation.sh

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -9,3 +9,6 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
+sudo userdmod -aG docker jenkins
+sudo systemctl restart docker.service
+sudo echo "jenkins ALL=ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
